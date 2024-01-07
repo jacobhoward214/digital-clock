999c7ef1763704144de9781b6f8196c82bb37b1a
jake.howard <jacobhoward214@gmail.com>
2024-01-06 22:31:35 -0500

    GUI clock created

--- digital-clock.py
+++ digital-clock.py
@@ -1,13 +1,34 @@
 from tkinter import *
 from tkinter import ttk
+import datetime
+
 root = Tk()
 frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-# btn = ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-# print(btn.configure().keys())
-# print(set(btn.configure().keys()) - set(frm.configure().keys()))
-# print(dir(btn))
-# print(set(dir(btn)) - set(dir(frm)))
+# frm.grid()
+# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+lab = Label(root)
+lab.pack()
+
+
+def clock():
+    time = datetime.datetime.now().strftime("Time: %H:%M:%S")
+    lab.config(text=time)
+    root.after(1000, clock)
+
+
+clock()
+# lbl = ttk.Label(frm, text=datetime.now()).grid(column=0, row=1)
+
+
+# Get list of configuration options
+# print(lbl.configure().keys())
+
+# Get specific configuration options by comparing to a simpler widget
+# print(set(lbl.configure().keys()) - set(frm.configure().keys()))
+
+# Get all methods and you can identify spific methods for the widget class
+# print(dir(lbl))
+# print(set(dir(lbl)) - set(dir(frm)))
 root.mainloop()
